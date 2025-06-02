ded898346f2c04e255ff17129df589dedb194cea
SvelteRust <oddharald@myhren.ai>
2025-06-02 18:01:24 +0200

    heh

--- lib/project_web/live/project_live.ex
+++ lib/project_web/live/project_live.ex
@@ -3,6 +3,10 @@ defmodule ProjectWeb.ProjectLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, socket |> assign(count: 0)}
+  end
+
+  def handle_event("increment", _params, socket) do
+    {:noreply, socket |> assign(count: socket.assigns.count + 1)}
   end
 end
